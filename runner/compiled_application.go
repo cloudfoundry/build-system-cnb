a3dd0deefe3ded6a554b35e772f6dc7b7c279b10
Ben Hale <bhale@pivotal.io>
2018-12-02 09:23:08 -0500

    Polishing
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- runner/compiled_application.go
+++ runner/compiled_application.go
@@ -1,9 +1,17 @@
 /*
- * Copyright 2018, Pivotal Software, Inc. All Rights Reserved.
- * Proprietary and Confidential.
- * Unauthorized use, copying or distribution of this source code via any medium is
- * strictly prohibited without the express written consent of Pivotal Software,
- * Inc.
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package runner
@@ -18,6 +26,7 @@ import (
 
 // CompiledApplication represents metadata about a compiled application.
 type CompiledApplication struct {
+	// JavaVersion is the version of Java used to compile the application.
 	JavaVersion string `toml:"java-version"`
 }
 
@@ -42,5 +51,13 @@ func javaVersion(application application.Application, executor Executor, logger 
 		return "", err
 	}
 
-	return strings.TrimSpace(string(v)), nil
+	s := strings.Split(strings.TrimSpace(string(v)), " ")
+	switch len(s) {
+	case 2:
+		return s[1], nil
+	case 1:
+		return s[0], nil
+	default:
+		return "unknown", nil
+	}
 }
